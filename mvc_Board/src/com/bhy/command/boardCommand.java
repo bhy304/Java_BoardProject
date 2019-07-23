c667c6fb4aafefd8d1dce8a2c9b7c465d728545e
bhy304 <bhy0512@gmail.com>
2019-07-23 23:47:44 +0900

    delete mvc_board folder

--- mvc_Board/src/com/bhy/command/boardCommand.java
+++
@@ -1,9 +0,0 @@
-package com.bhy.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface boardCommand {
-
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
